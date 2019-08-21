18123c505f825e54eaadace99cd9620636ffb8ce
Sydnee Sampson <sbsydnee161416@gmail.com>
2019-08-21 00:42:12 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -5,4 +5,10 @@ def oxford_comma(array)
     return array[0]
     elsif array.length == 2 
     return "#{array[0]} and #{array[1]}"
+  else 
+    until i == array.length - 1 
+    s << "#{array}[i]}, "
+    i += 1 
+  end 
+  
 end
